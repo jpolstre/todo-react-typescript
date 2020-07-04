508821d0497f9be1c56e954ac7661ae4d516bf11
jpolstre <jpolstre@gmail.com.com>
2020-07-04 12:59:28 -0400

    Updates

---
+++ precache-manifest.197a2b230b5a5dd9418595403a4b7fac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9847a7260d931b87b289cf2f873f2592",
+    "url": "/todo-react-typescript/index.html"
+  },
+  {
+    "revision": "e48e7a394f8e97b0d6f5",
+    "url": "/todo-react-typescript/static/js/2.c9112715.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/todo-react-typescript/static/js/2.c9112715.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a643c8b8225c17199f4",
+    "url": "/todo-react-typescript/static/js/main.6aae7982.chunk.js"
+  },
+  {
+    "revision": "fa5be56a13e36b05378a",
+    "url": "/todo-react-typescript/static/js/runtime-main.6caf57dc.js"
+  }
+]);
